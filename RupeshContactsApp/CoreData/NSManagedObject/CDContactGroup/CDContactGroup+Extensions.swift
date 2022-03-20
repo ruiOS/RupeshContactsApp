cf63a2b9cfb943d9829d5b654beff4ebed27f31e
Rupesh <rupeshmonikaraju@zohomail.in>
2022-03-20 17:07:57 +0530

    test yml

--- RupeshContactsApp/CoreData/NSManagedObject/CDContactGroup/CDContactGroup+Extensions.swift
+++ RupeshContactsApp/CoreData/NSManagedObject/CDContactGroup/CDContactGroup+Extensions.swift
@@ -20,4 +20,21 @@ extension CDContactGroup: NSManagedObjectEntityProtocol {
         return NSFetchRequest<CDContactGroup>(entityName: entityName)
     }
 
+    ///method returns contact value of cdContact
+//    func convertToContactGroup() -> Contact{
+//        Contact(contactNumber: self.contactNumber, contactPic: self.contactPic, firstName: self.firstName, lastName: self.lastName, middleName: self.middleName, id: self.id)
+//    }
+
+    /// method binds contact value to self
+    /// - Parameter contact: contact to be binded to self
+//    func bind(contact: Contact){
+//        self.contactNumber = contact.contactNumber
+//        self.contactPic = contact.contactPic
+//        self.firstName = contact.firstName
+//        self.id = contact.id
+//        self.lastName = contact.lastName
+//        self.middleName = contact.middleName
+//
+//        PersistentStorage.shared.saveContext()
+//    }
 }
